ade50c6a4f7ba4b514c7875635a7cbc2478636e8
Al Garnand <al.garnand@gmail.com>
2014-01-30 15:29:30 -0600

    Initial commit

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
+require 'rails/all'
+
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#frequire 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
